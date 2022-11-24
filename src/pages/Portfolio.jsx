b6210a37cb377734280f73d8adde5da97ba365e5
Laylla Emily <layllandradef@live.com>
2022-11-24 12:38:41 -0300

    Criação primeira parte do portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,40 @@
 import { Header } from "../components/Header";
 import portfolioImg from '../assets/portfolio.svg'
+import primeiroImg from '../assets/primeiro.png'
+import maravilhosaImg from '../assets/maravilhosa.png'
+import netflixImg from '../assets/netflix.png'
+
+import styles from '../styles/components/pages/portfolio.module.css'
 
 
 export function Portfolio() {
     return(
-        <Header title="Meus Projetinhos" image={portfolioImg} />   
+        <div>
+        <Header title="Meus Projetinhos" image={portfolioImg} />
+        <div className={styles.projetosFavoritos}>
+            <h2 className={styles.tituloPrincipal}>Favoritos</h2>
+        <div className={styles.containerCards}>
+            <div className={styles.cards}></div>
+                <h1></h1>
+                <img className={styles.cardImg} src={primeiroImg}/>
+                <p></p>
+                <a className={styles.cardLink}></a>
+            <div className={styles.cards}>
+                <div className={styles.cards}></div>
+                <h1></h1>
+                <img className={styles.cardImg} src={maravilhosaImg}/>
+                <p></p>
+                <a className={styles.cardLink}></a>
+            </div>
+            <div className={styles.cards}>
+                <div className={styles.cards}></div>
+                <h1></h1>
+                <img className={styles.cardImg} src={netflixImg}/>
+                <p></p>
+                <a className={styles.cardLink}></a>
+            </div>
+        </div>
+        </div>
+    </div>
     )
 }
