e594b6ddef8fc17e21440fe447939f427c60b439
Laylla Emily <layllandradef@live.com>
2022-11-24 14:17:52 -0300

    Adicionando Readme

---
+++ README.md
@@ -0,0 +1,69 @@
+# Projeto Guiado - ReactJS🚀 
+Turma Online 20 - Todas em Tech  | Front-end | Semana 15 | 2022 | { Profa } Simara Conceição
+
+## Projeto
+### `Meu site pessoal`
+
+> ⚠️ Neste site, eu treino a aplicação dos conceitos fundamentais de ReactJS que aprendi durante o módulo de ReactJS da Reprograma.
+
+# 🧠 Contexto
+
+O objetivo era criar um site usando ReactJS, onde eu pudesse me apresentar e enriquecer o meu portfólio.
+
+# Tecnologias usadas
+ * React.js
+
+É uma biblioteca usada com a linguagem de programação JavaScript no desenvolvimento Front-end. 
+O Facebook criou o React em 2011 e disponibilizou a tecnologia em código aberto em 2013. Desde então, ela é uma das bibliotecas mais populares entre os desenvolvedores. Sendo uma biblioteca, o React.js é um conjunto de funções padronizadas. Os desenvolvedores a utilizam para construir uma aplicação. 
+Neste caso específico, a utilização se dá no desenvolvimento de interfaces de usuário UI (User Interface). Por essa razão, o emprego se dá exclusivamente no desenvolvimento Front-end. 
+Quando os desenvolvedores do Facebook criaram esta ferramenta, o objetivo era otimizar a atualização do feed, mensagens do chat, status e listagem de contatos de forma mais dinâmica e rápida. 
+Isso acontece porque a conexão entre JavaScript, CSS e HTML, juntamente com os demais componentes, é simplificada.  
+
+* Vite 
+
+Vite é uma ferramenta de construção que foi inicialmente desenvolvida para Vue.js. Com a nova atualização, o Vite agora oferece suporte à maioria das estruturas da web.
+O Vite fornece código-fonte em ESM nativo . As importações dinâmicas condicionais são processadas apenas quando usadas pela tela atual requer a importação. O navegador faz a tarefa de agrupar o código-fonte. O Vite só serve e transforma o código-fonte sob demanda, conforme solicitado pelo navegador.
+
+* Npm
+
+O NPM, que significa Node Package Manager, é um poderoso gerenciador de pacotes que faz parte do Node.js. Esse, por sua vez, é um ambiente para a execução de JavaScript no lado do servidor de hospedagem. Em outras palavras, ele permite utilizar a linguagem JavaScript no back-end da aplicação.
+Trata-se de uma ferramenta crucial para realizar um desenvolvimento organizado e otimizado, pois permite instalar dependências em um projeto, mantê-las atualizadas e muito mais. Por isso, é importante conhecer essa tecnologia e entender como utilizá-la. 
+
+* Module CSS
+
+Segundo a documentação disponível no repositório oficial no github: são arquivos CSS nos quais todos os nomes de classes e animações são carregados em escopo local por padrão.
+Ou seja, o css-modules é um processo que ocorre durante o passo de build, que altera os nomes de classes e seletores para que tenham um escopo local. 
+
+* React Router Dom
+
+Pensando que ao utilizar o React para o desenvolvimento de uma aplicação padrão SPA (Single Page Application) e que nessa aplicação seja possível acessar outras interfaces e componentes, há a necessidade de fazer o roteamento dos caminhos que serão acessados. A maneira mais completa de fazer isso e também uma das mais utilizadas é com a biblioteca chamada react-router, que possui uma versão web chamada react-router-dom.
+O react-router-dom é uma biblioteca padrão para que você consiga fazer o roteamento das páginas da sua aplicação de forma dinâmica.
+
+* React Axios
+
+Axios é um cliente HTTP baseado em Promises para fazer requisições. Pode ser utilizado tanto no navegador quanto no Node.js ou qualquer serviço de API. Ele é uma biblioteca leve e também uma alternativa para quem usa o fetch api.
+
+* Vercel 
+
+A Vercel é uma plataforma voltada para a hospedagem de aplicações de uma forma bem simples e rápida. Ela é conhecida por ser a empresa criadora do framework Next JS, voltado para o React. Como diz seu próprio site “A Vercel é o melhor lugar para implantar qualquer aplicativo de front-end.”
+
+* Github 
+
+É um serviço baseado em nuvem que hospeda um sistema de controle de versão (VCS) chamado Git. Ele permite que os desenvolvedores colaborem e façam mudanças em projetos compartilhados enquanto mantêm um registro detalhado do seu progresso.
+
+### Como rodar esse projeto localmente
+
+* Faça o fork
+* Clone o link do repositorio em seu git bash -> (git clone link-do-repositório)
+* Baixe as dependencias -> (npm i)
+* Rode o projeto -> (npm run dev)
+
+### Melhorias Futuras
+
+* Incluir Testes
+* Revisar acessibilidade
+* Responsividade (Criação de media queries: mobile, tablet, tablet landscape e laptops)
+
+<p align="center">
+Feito com 💜 por Laylla Andrade
+</p>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,31 +9,69 @@ import styles from '../styles/components/pages/portfolio.module.css'
 
 export function Portfolio() {
     return(
-        <div>
-            <Header title="Meus Projetinhos" image={portfolioImg} />
-            <div className={styles.projetosFavoritos}>
-                <h2 className={styles.tituloPrincipal}>Favoritos</h2>
-                <div className={styles.containerCards}>
-                    <div className={styles.cards}>
-                        <h1>Primeiro Projeto</h1>
-                        <img className={styles.cardImg} src={primeiroImg}/>
-                        <p>Projeto muito especial para mim,feito apenas com html e css. Realizado na seleção do curso, foi minha porta de entrada no Reprograma</p>
-                        <a className={styles.cardLink} href="https://ws3-laylla.netlify.app/" target="_blank">Ver projeto</a>
-                    </div>
-                    <div className={styles.cards}>
-                        <h1>Projeto M de maravilhosa</h1>
-                        <img className={styles.cardImg} src={maravilhosaImg}/>
-                        <p>Projeto realizado no reprograma, utilizando html e css. Onde também foi adicionado media queries, para que o projeto se tornasse responsivo.</p>
-                        <a className={styles.cardLink} href="https://projeto-laylla.netlify.app/" target="_blank">Ver projeto</a>
-                    </div>
-                    <div className={styles.cards}>
-                        <h1>Projeto Netflix</h1>
-                        <img className={styles.cardImg} src={netflixImg}/>
-                        <p>Projeto que teve como base a criação de tela, que mostrasse protagonistas trans em series.</p>
-                        <a className={styles.cardLink} href="https://projetonetflixx.netlify.app/" target="_blank">Ver projeto</a>
+        <>
+            <div>
+                <Header title="Meus Projetinhos" image={portfolioImg} />
+                <div className={styles.projetosFavoritos}>
+                    <h2 className={styles.tituloPrincipal}>Favoritos</h2>
+                    <div className={styles.containerCards}>
+                        <div className={styles.cards}>
+                            <h1>Primeiro Projeto</h1>
+                            <img className={styles.cardImg} src={primeiroImg}/>
+                            <p>Projeto muito especial para mim, feito apenas com html e css. Realizado na seleção do curso, foi minha porta de entrada no Reprograma</p>
+                            <a className={styles.cardLink} href="https://ws3-laylla.netlify.app/" target="_blank">Ver projeto</a>
+                        </div>
+                        <div className={styles.cards}>
+                            <h1>Projeto M de maravilhosa</h1>
+                            <img className={styles.cardImg} src={maravilhosaImg}/>
+                            <p>Projeto realizado no reprograma, utilizando html e css. Onde também foi adicionado media queries, para que o projeto se tornasse responsivo.</p>
+                            <a className={styles.cardLink} href="https://projeto-laylla.netlify.app/" target="_blank">Ver projeto</a>
+                        </div>
+                        <div className={styles.cards}>
+                            <h1>Projeto Netflix</h1>
+                            <img className={styles.cardImg} src={netflixImg}/>
+                            <p>Projeto que teve como base a criação de tela, que mostrasse protagonistas trans em series.</p>
+                            <a className={styles.cardLink} href="https://projetonetflixx.netlify.app/" target="_blank">Ver projeto</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className={styles.projetsContainer}>
+                <h2 className={styles.projectsTitle}>Outros projetos no meu Github</h2>
+                <div className={styles.projetsContainer}>
+                <div className={styles.cardsRepoContainer}>
+                    <div className={styles.cardRepo}>
+                        <h3 className={styles.cardRepoText}>Titulo</h3>
+                        <p className={styles.cardRepoText}>Descrição</p>
+                    </div>
+                    <div className={styles.cardRepo}>
+                        <h3 className={styles.cardRepoText}>Titulo</h3>
+                        <p className={styles.cardRepoText}>Descrição</p>
+                    
+                    </div>
+                    <div className={styles.cardRepo}>
+                        <h3 className={styles.cardRepoText}>Titulo</h3>
+                        <p className={styles.cardRepoText}>Descrição</p>
+                    
+                    </div> 
+                    <div className={styles.cardRepo}>
+                        <h3 className={styles.cardRepoText}>Titulo</h3>
+                        <p className={styles.cardRepoText}>Descrição</p>
+                    
+                    </div>
+                    <div className={styles.cardRepo}>
+                        <h3 className={styles.cardRepoText}>Titulo</h3>
+                        <p className={styles.cardRepoText}>Descrição</p>
+                    
+                    </div>
+                    <div className={styles.cardRepo}>
+                        <h3 className={styles.cardRepoText}>Titulo</h3>
+                        <p className={styles.cardRepoText}>Descrição</p>
+                    
+                    </div>
+                </div>
+                </div>
+            </div>
+        </>
     )
 }
